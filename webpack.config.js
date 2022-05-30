608362abd508d794eacbd6ec0bd16f86a5e206d8
JeremyGlize <JeremGlize@gmail.com>
2022-05-30 18:55:32 +0200

    modd files

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'prod');
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
 Encore
